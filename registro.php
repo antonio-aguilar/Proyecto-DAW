167340b4cad8f7e8d8695f1236ab44a0f51f35f5
Antonio Aguilar <55695218+antonio-aguilar@users.noreply.github.com>
2021-11-26 22:41:18 +0100

    Cambios

--- registro.php
+++ registro.php
@@ -15,46 +15,40 @@ encabezado();
 
 // Funciones necesarias
 function registro($bd) {
-    
-    $stmt = $db->prepare("INSERT INTO users(username,password,email,name) VALUES (:username,:hash_password,:email,:name)");
-    $stmt->bindParam("username", $username,PDO::PARAM_STR) ;
-    $hash_password= hash('sha256', $password); //Password encryption
-    $stmt->bindParam("hash_password", $hash_password,PDO::PARAM_STR) ;
-    $stmt->bindParam("email", $email,PDO::PARAM_STR) ;
-    $stmt->bindParam("name", $name,PDO::PARAM_STR) ;
-    $stmt->execute();
+  // Declaracion de variables para el envío de informacion
+  $usuario = $_POST['usuario'];
+  $contrasena = password_hash($_POST['contrasena'], PASSWORD_DEFAULT);
+  $nombre = $_POST['nombre'];
+  $email = $_POST['email'];
+      
+  // Sentencia que inserta la palabra en la base de datos
+  $sql = "INSERT INTO usuarios VALUES ($usuario,$contrasena,$nombre,$email)";
+  //echo $consulta;
 
-    INSERT INTO `usuarios` (`ID_USUARIO`, `USUARIO`, `CONTRASENA`, `NOMBRE`, `EMAIL`) VALUES (NULL, '', '', '', '')
-
-  if (!empty($_POST['usuario']) && !empty($_POST['contrasena']) && !empty($_POST['nombre']) && !empty($_POST['email'])) {
-    $sql = "INSERT INTO usuarios (usuario, contrasena, nombre, email) VALUES (:usuario, :contrasena, :nombre, :email)";
-    $stmt = $bd->prepare($sql);
-    $stmt->bindParam(':email', $_POST['email']);
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-    $stmt->bindParam(':password', $password);
-
-    if ($stmt->execute()) {
-      $message = 'Successfully created new user';
-    } else {
-      $message = 'Sorry there must have been an issue creating your account';
-    }
+  /*if ($usuario == $consulta) {
+    echo "No se puede insertar el usuario $usuario";
+  }*/
+  if ($sql = $bd->exec($sql)) {
+    echo ("Usuario registrado con éxito");
+  } else {
+    echo ("Ha habido un error insertando el usuario");
   }
 }
+
 // Titulo  
 echo "<h3>Registro de Usuario</h3>";
 
 // Formulario de registro de usuario
 echo "<form name=\"formularioRegistro\" action=\"registro.php\" method=\"post\">";
 echo "<p>Usuario: <input type=\"text\" name=\"usuario\" required></p>";
-echo "<p>Contraseña: <input type=\"password\" name=\"password\" required></p>";
-echo "<p>Contraseña: <input type=\"password\" name=\"repassword\" required></p>";
+echo "<p>Contraseña: <input type=\"password\" name=\"contrasena\" required></p>";
 echo "<p>Nombre: <input type=\"text\" name=\"nombre\" required></p>";
 echo "<p>Email: <input type=\"email\" name=\"email\" required></p>";
 echo "<p><input class=\"boton_personalizado\" type=\"submit\" name=\"registro\" value=\"registro\"></p>";
 echo "</form>";
 
 if (isset($_POST["registro"])) {
-    registro($bd);
+  registro($bd);
 }
 
 // Pie de la web
